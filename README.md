4e444ba46097a6166c5961deef35ae13384d9ad2
Jonathan Lloyd <jonathan@thisisjonathan.com>
2022-07-09 13:35:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Toy 3D engine
+Learning project to build a simple 3D engine that can render basic wireframes
---
+++ src/App.ts
@@ -0,0 +1,4 @@
+window.onload = () => {
+  let title = document.querySelector("#title");
+  if (title) title.innerHTML = 'Hello World';
+};
